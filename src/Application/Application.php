ed59c4ec5b441326e6f7b0649c58f07e6a33fd2d
Jleagle <jimeagle@gmail.com>
2015-06-18 21:33:12 +0100

    Use AssetManager for CSS & JS

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -6,9 +6,34 @@ use Jleagle\Packages\Application\Controllers\InfoController;
 use Jleagle\Packages\Application\Controllers\PackageController;
 use Jleagle\Packages\Application\Controllers\SearchController;
 use Jleagle\Packages\Application\Controllers\StatsController;
+use Packaged\Dispatch\AssetManager;
 
 class Application extends ApplicationKernel
 {
+  protected function _init()
+  {
+    $am = AssetManager::aliasType('src');
+
+    $am->requireCss(
+      [
+        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css',
+        'css/jquery.select2',
+        'css/jquery.select2-bootstrap',
+        'css/main',
+      ]
+    );
+
+    $am->requireJs(
+      [
+        '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
+        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js',
+        'js/jquery.select2.min',
+        'js/jquery.highlight',
+        'js/main',
+      ]
+    );
+  }
+
   public function getRoutes()
   {
     return [
